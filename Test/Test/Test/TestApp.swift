07dfc191a5fc4752410d669ffa0811f313862951
Kawaji Mizuki <>
2026-01-22 19:16:37 +0900

    課題のコードをGithub上に登録

---
+++ Test/Test/Test/TestApp.swift
@@ -0,0 +1,18 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Kawaji Mizuki on 2026/01/20.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // 既存の ContentView をルートに
+            
+        }
+    }
+}
